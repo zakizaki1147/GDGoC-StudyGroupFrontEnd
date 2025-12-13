0275cb27545369901115e7941eae99d722a3ad53
zakizaki1147 <zakisantoso47@gmail.com>
2025-12-14 05:55:03 +0700

    feat: money logic

--- tugas.js
+++ tugas.js
@@ -4,7 +4,12 @@ const nama = input("Masukkan nama: ")
 const umur = Number(input("Masukkan umur: "))
 
 if (umur >= 21) {
-	//
+	const uang = Number(input("Masukkan jumlah uang yang dibawa: "))
+	if (uang < 500000) {
+		console.log("Maaf " + nama + ", uang kamu cuma " + uang + ", datang lagi lain kali")
+	} else {
+		console.log("Selamat datang, " + nama)
+	}
 } else {
 	console.log("Maaf " + nama + ", umur kamu kurang " + (21 - umur))
 }
